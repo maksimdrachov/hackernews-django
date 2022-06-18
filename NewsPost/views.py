b4b82740eec6018b00f831999a09d379daad45f2
Maksim Drachov <maksim.nesterenko@unikie.com>
2022-06-18 20:16:30 +0300

    timezone

--- NewsPost/views.py
+++ NewsPost/views.py
@@ -5,7 +5,7 @@ from .forms import NewsPostForm
 
 # Create your views here.
 def news_post_detail_view(request):
-    obj = NewsPost.objects.get(id=1)
+    obj = NewsPost.objects.get(id=2)
     context = {
         'object' : obj
     }
