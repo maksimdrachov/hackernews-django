a816bae7a0bd0a2f1ff66d0029efceff7f985819
Maksim Drachov <maksim.drachov@outlook.com>
2022-06-25 14:20:36 +0300

    comments

--- NewsPost/views.py
+++ NewsPost/views.py
@@ -3,11 +3,15 @@ from django.shortcuts import render
 from .models import NewsPost
 from .forms import NewsPostForm
 
+from Comment.models import Comment
+
 # Create your views here.
 def news_post_detail_view(request, my_id):
     obj = NewsPost.objects.get(id=my_id)
+    comment = Comment.objects.all()
     context = {
-        'object' : obj
+        'object' : obj,
+        'comment': comment,
     }
     return render(request, "NewsPost/detail.html", context)
 
