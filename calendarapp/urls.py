3e5d179c3a030549c0905e133d133a13576ab025
Maksim Drachov <maksim.drachov@outlook.com>
2022-07-03 14:17:11 +0300

    event create

--- calendarapp/urls.py
+++ calendarapp/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     #path("calender/", views.CalendarViewNew.as_view(), name="calendar"),
     #path("calenders/", views.CalendarView.as_view(), name="calendars"),
     path("events/", views.SimpleCalendarView.as_view(), name="events"),
-    #path("event/new/", views.create_event, name="event_new"),
+    path("event/new/", views.create_event, name="event_new"),
     #path("event/edit/<int:pk>/", views.EventEdit.as_view(), name="event_edit"),
     #path("event/<int:event_id>/details/", views.event_details, name="event-detail"),
     #path(
